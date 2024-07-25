f189c5b61b307e5563930a3da5be76e996d810be
Francisco Cordoba <pako.cordoba10@gmail.com>
2024-07-25 11:43:16 -0600

    First commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Francisco Cordoba on 24/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
